a6670df10008598a689b151816d64fde4ac94b84
lucia-zy-wang <161808164+lucia-zy-wang@users.noreply.github.com>
2025-05-03 20:25:57 +0930

    Added array to track individual pkt acknowledgement status. Tested full window behaviour with high sending rate (2), pkt were ignored when window is full.

--- sr.c
+++ sr.c
@@ -61,6 +61,7 @@ static struct pkt buffer[WINDOWSIZE];  /* array for storing packets waiting for 
 static int windowfirst, windowlast;    /* array indexes of the first/last packet awaiting ACK */
 static int windowcount;                /* the number of packets currently awaiting an ACK */
 static int A_nextseqnum;               /* the next sequence number to be used by the sender */
+static int acked[WINDOWSIZE]           /* array for storing packets ACKed status (0 unacked /1 acked)*/
 
 /* called from layer 5 (application layer), passed the message to be sent to other side */
 void A_output(struct msg message)
